a56cc1ac18a234bd0c22ed6d8f37f733e92c5464
manuel <manuel@Manuel>
2019-06-24 11:05:53 +0200

    I save my ClienteServiceImpl

--- my_spring_knowledge_now1/src/main/java/com/bolsadeideas/springboot/app/models/service/ClienteServiceImpl.java
+++ my_spring_knowledge_now1/src/main/java/com/bolsadeideas/springboot/app/models/service/ClienteServiceImpl.java
@@ -2,19 +2,24 @@ package com.bolsadeideas.springboot.app.models.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.bolsadeideas.springboot.app.models.dao.IClienteDao;
 import com.bolsadeideas.springboot.app.models.dao.entity.Cliente;
 
 @Service
 public class ClienteServiceImpl implements IClienteService {
 
+	@Autowired
+	private IClienteDao clienteDao;
+	
 	@Override
 	@Transactional(readOnly = true)
 	public List<Cliente> searchForAll() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return (List<Cliente>)clienteDao.findAll();
 	}
 
 }
